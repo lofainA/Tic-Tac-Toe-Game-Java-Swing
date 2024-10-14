25cf39b272a1b221b19ad46bff5dbc2d26cfcdc8
Aysha Idris <lofain968@gmail.com>
2024-10-14 11:13:58 +0530

    Added: Restart button

--- GamePanel.java
+++ GamePanel.java
@@ -2,8 +2,12 @@ package main;
 
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -12,9 +16,11 @@ public class GamePanel extends JPanel{
 	int n = 3;
 	int move = 0;
 	
+	ButtonGroup cells = new ButtonGroup();
 	State[][] board = new State[n][n];
 	JPanel gameBoard = new JPanel();
 	JLabel titleCard = new JLabel("Tic-Tac-Toe");
+	JButton restart = new JButton("Restart");
 	
 	GamePanel() {
 		
@@ -28,13 +34,18 @@ public class GamePanel extends JPanel{
 				board[i][j] = State.Blank;
 				Cell cell = new Cell(this, i, j);
 				gameBoard.add(cell);
+				cells.add(cell);
 			}
 		}
 		
 		this.add(gameBoard);
+		
+		restart.addActionListener(e -> restart());
+		this.add(restart);
 	}
 	
 	public void checkForWin(int x, int y, State s) {
+		
 		// column win
 		for(int i = 0; i < n; i++) {
 			if(board[x][i] != s) {
@@ -88,7 +99,7 @@ public class GamePanel extends JPanel{
 			}
 		}
 		
-		
+		// match draw
 		if(move == n*n) {
 			titleCard.setText("ITS A TIEEE");
 		}
@@ -98,5 +109,29 @@ public class GamePanel extends JPanel{
 		for(int i = 0; i < n * n; i++) {
 			gameBoard.getComponent(i).setEnabled(false);;
 		}
+	}
+	
+	public void restart() {
+		
+		// make the buttons of the button group an iterable
+		Enumeration<AbstractButton> buttons = cells.getElements();
+		
+		// reset text of all buttons
+		while(buttons.hasMoreElements()) {
+			AbstractButton btn = buttons.nextElement();
+			btn.setText("");
+			btn.setEnabled(true);
+		}
+		
+		// reset values of board matrix to state blank
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				board[i][j] = State.Blank;
+				System.out.println(board[i][j]);
+			}
+		}
+
+		titleCard.setText("Tic-Tac-Toe"); // reset title card text
+		move = 0; // reset move count to zero
 	}
 }
--- Main.java
+++ Main.java
@@ -2,28 +2,29 @@ package main;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main {
+	static JFrame frame =  new JFrame();
 	public static void main(String[]args) {
 		
 		int frameWidth = 450;
-        int frameHeight = 540;
-        
-		JFrame frame =  new JFrame();
-		
-		Container panel = frame.getContentPane();
-        panel.setBackground(Color.BLACK);
+        int frameHeight = 600;
         
         GamePanel gp = new GamePanel();
         frame.add(gp);
         
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setSize(frameWidth, frameHeight);
         frame.setTitle("Tic-tac-toe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
+       
 	}
 }
